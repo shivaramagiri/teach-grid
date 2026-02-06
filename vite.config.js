5bedd41a47043705cc30b6b8d16b61e5ccb6ff46
shivaramagiri <shiva.ui.91@gmail.com>
2026-02-06 19:46:45 +0530

    all changes done

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,6 @@ import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/teach-grid/",
-  plugins: [react(), ghPages()],
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx'
-      }
-    }
-  }
+  base: "/",
+  plugins: [react()],
 })
